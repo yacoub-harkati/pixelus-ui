19fc333f6f74a8d161dcbf6ba1b3f18a81de0690
Yacoub Harkati <haryacoub@gmail.com>
2023-03-24 22:00:03 +0000

    added caroussel support

--- components/Carroussel.tsx
+++ components/Carroussel.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image"
 import CarrousselBanner from "@/assets/carroussel-banner.png"
 import IconButton from "@/components/ui/IconButton"
-import React from "react"
+import React, { useState } from "react"
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
@@ -14,7 +14,7 @@ import useMediaQuery from "@/hooks/useMediaQuery"
 
 type Props = {}
 
-export function CarrousselItem({}: Props) {
+export function Slide({}: Props) {
   return (
     <div className="relative grid h-[520px] shrink-0 basis-full snap-center md:grid-cols-6">
       <div className="md:col-span-4">
@@ -24,7 +24,7 @@ export function CarrousselItem({}: Props) {
           alt="Banner-Image"
         />
       </div>
-      <div className="flex h-full flex-col gap-4 px-6 py-6 sm:px-9 md:col-span-2 md:pt-28">
+      <div className="flex h-full flex-col gap-4 px-6 py-6 ring-1 ring-white dark:ring-stroke-dark sm:px-9 md:col-span-2  md:pt-28">
         <p className="text-3xl font-semibold sm:text-4xl">WITCASTS</p>
         <p className="max-w-[80%] text-base sm:text-xl">
           Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -49,14 +49,17 @@ export function CarrousselItem({}: Props) {
 
 export default function Carroussel() {
   const isAboveMediumScreen = useMediaQuery("(min-width: 768px)")
+  const [activeSlide, setactiveSlide] = useState(1)
+
+  function handleSlide() {}
 
   return (
     <div className="relative overflow-hidden rounded-3xl bg-secondary-light shadow-lg ring-1 ring-white dark:bg-secondary-dark dark:ring-stroke-dark">
       <div className="flex snap-x snap-mandatory overflow-y-auto scrollbar-hide">
-        <CarrousselItem />
-        <CarrousselItem />
-        <CarrousselItem />
-        <CarrousselItem />
+        <Slide />
+        <Slide />
+        <Slide />
+        <Slide />
       </div>
       {isAboveMediumScreen && (
         <div className="absolute top-1/2 z-40 flex w-full -translate-y-1/2 transform justify-between px-4">
